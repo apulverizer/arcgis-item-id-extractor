aca67f9cb3172d8d6e8ff44be69bf706357f53fd
Aaron Pulver <apulver@esri.com>
2018-05-17 21:21:37 -0400

    add support for workforce and for portals

--- arcgis_item_id_extractor.js
+++ arcgis_item_id_extractor.js
@@ -2,56 +2,68 @@
 
 // A callback function to handle the click
 function copyItemId(info, tab) {
-  if (info.linkUrl.includes('webmap=')){
-    copyTextToClipboard(getUrlParam('webmap',info.linkUrl));
+    if (info.linkUrl.includes('webmap=')){
+      copyTextToClipboard(getUrlParam('webmap',info.linkUrl));
+    }
+    else if (info.linkUrl.includes('/opsdashboard/')){
+      copyTextToClipboard(getDashboardId(info.linkUrl));
+    }
+    else if (info.linkUrl.includes('/workforce/')){
+      copyTextToClipboard(getWorkforceId(info.linkUrl));
+    }
+    else{
+      copyTextToClipboard(getUrlParam('id',info.linkUrl));
+    }
   }
-  else if (info.linkUrl.includes('opsdashboard')){
-    copyTextToClipboard(getDashboardId(info.linkUrl));
+  
+  function getWorkforceId(url){
+      return url.split("/").slice(-3,-2);
   }
-  else{
-    copyTextToClipboard(getUrlParam('id',info.linkUrl));
+  
+  function getDashboardId(url){
+    if (url.includes('/new')){
+      return getUrlParam('id',url);
+    }
+    else{
+      return url.split("#")[1].split("/")[1].split("?")[0];
+    }
   }
-}
-
-function getDashboardId(url){
-  if (url.includes('/new')){
-    return getUrlParam('id',url);
+  
+  // extract a parameter from a url
+  // http://www.netlobo.com/url_query_string_javascript.html
+  function getUrlParam( name, url ) {
+      name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+      var regexS = "[\\?&]"+name+"=([^&#]*)";
+      var regex = new RegExp( regexS );
+      var results = regex.exec( url );
+      return results == null ? null : results[1];
   }
-  else{
-    return url.split("#")[1].split("/")[1].split("?")[0];
+  
+  // copies some text to the clipboard by creating a DOM element,
+  // copying the text in it, then removing the element
+  function copyTextToClipboard(text) {
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = text;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
   }
-}
-
-// extract a parameter from a url
-// http://www.netlobo.com/url_query_string_javascript.html
-function getUrlParam( name, url ) {
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var regexS = "[\\?&]"+name+"=([^&#]*)";
-    var regex = new RegExp( regexS );
-    var results = regex.exec( url );
-    return results == null ? null : results[1];
-}
-
-// copies some text to the clipboard by creating a DOM element,
-// copying the text in it, then removing the element
-function copyTextToClipboard(text) {
-  var copyFrom = document.createElement("textarea");
-  copyFrom.textContent = text;
-  var body = document.getElementsByTagName('body')[0];
-  body.appendChild(copyFrom);
-  copyFrom.select();
-  document.execCommand('copy');
-  body.removeChild(copyFrom);
-}
-
-// Create the context menu, allow item-details page and webmap viewer links
-var contextMenu = chrome.contextMenus.create({
-    "title": "Copy item-id to clipboard",
-    "contexts":["link"],
-    "onclick": copyItemId,
-    "targetUrlPatterns": [
-        "*://*.arcgis.com/home/item.html?id=*",
-        "*://*.arcgis.com/home/webmap/viewer.html?webmap=*",
-        "*://*.arcgis.com/apps/opsdashboard/index.html*"
-    ]
-});
+  
+  // Create the context menu, allow item-details page and webmap viewer links
+  var contextMenu = chrome.contextMenus.create({
+      "title": "Copy item-id to clipboard",
+      "contexts":["link"],
+      "onclick": copyItemId,
+      "targetUrlPatterns": [
+          "*://*/home/item.html?id=*",
+          "*://*/home/webmap/viewer.html?webmap=*",
+          "*://*/apps/opsdashboard/index.html*",
+          "*://*/*/home/item.html?id=*",
+          "*://*/*/home/webmap/viewer.html?webmap=*",
+          "*://*/*/apps/opsdashboard/index.html*",
+          "*://*/*/apps/workforce/*",
+          "*://*/projects/*/dispatch/assignments"
+      ]
+  });
